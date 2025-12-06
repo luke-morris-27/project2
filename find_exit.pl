638ff59b6d2ece06d493633b7eaac0914ff87cc3
luke-morris-27 <lukemorristx@outlook.com>
2025-12-05 19:29:56 -0600

    Add follow_moves and legal predicates

--- find_exit.pl
+++ find_exit.pl
@@ -14,3 +14,14 @@ find_start(Maze, Row, Col) :-
 cell(Maze, Row, Col, Cell) :-
     nth0(Row, Maze, RowList),
     nth0(Col, RowList, Cell).
+
+follow_moves(_, [], R, C, R, C).
+
+follow_moves(Maze, [A|Rest], R0, C0, RF, CF) :-
+    move(Maze, A, R0, C0, R1, C1),
+    follow_moves(Maze, Rest, R1, C1, RF, CF).
+
+legal(Maze, R, C) :-
+    cell(Maze, R, C, Cell),
+    Cell \= w. % cannot go into wall
+
